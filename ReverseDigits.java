2cff255baf6926ded5fd8320c20612f60e947bcf
Sanjay Premkumar P <130763766+sanjay-premkumar@users.noreply.github.com>
2026-06-20 13:53:06 +0530

    reverse sum file added

---
+++ ReverseDigits.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ReverseDigits {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        while (num > 0) {
+            int digit = num % 10;
+            System.out.print(digit + " ");
+            num = num / 10;
+        }
+    }
+}
